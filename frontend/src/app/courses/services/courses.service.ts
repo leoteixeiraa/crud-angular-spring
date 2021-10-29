accf0fc4811224b6585b4a9e79009304e32d301f
Leonardo Teixeira <leovenancio00@gmail.com>
2021-10-28 21:47:06 -0300

    feat: adding spninner loading

--- frontend/src/app/courses/services/courses.service.ts
+++ frontend/src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,8 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
+      first(),
+      delay(5000),
       tap(courses => console.log(courses))
     );
   }
